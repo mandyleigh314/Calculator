3464f1a96afdd94913cbedda8ffd3bc8c9d0a60b
Mandy Jacobsen <Mandy@Knightowltrivia.com>
2017-07-14 10:14:23 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+let four = document.getElementById('four')
+let five = document.getElementById('five')
+let six = document.getElementById('six')
+let seven = document.getElementById('eight')
+let nine = document.getElementById('nine')
+let zero = document.getElementById('zero')
+let clear = document.getElementById('clear')
+let multiply = document.getElementById('multiply')
+let divide = document.getElementById('divide')
+let subtract = document.getElementById('subtract')
+let add = document.getElementById('add')
+
+function test() {
+  alert('test')
+}
